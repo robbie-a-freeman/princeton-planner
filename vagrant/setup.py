3f6e5568936fba22a6728f29b87be5af6b35a184
wsweeny <wsweeny@princeton.edu>
2018-04-05 18:05:22 -0400

    Simplifying directory structure/setup greatly
    Vagrant now runs in the git directory, and uses gitignore to avoid syncing files. We share the project directory as "/vagrant" within the VM.

--- vagrant/setup.py
+++ vagrant/setup.py
@@ -2,29 +2,8 @@ import shutil
 import os
 from subprocess import call
 
-targetPath = "./../../vagrant"
-
 def main():
-    # overwrite existing vagrant files.
-    if (os.path.isdir(targetPath)):
-        reply = input("The vagrant folder already exists. Overwrite? (y/n)")
-        if (reply == 'y' or reply == 'Y'):
-            # print(targetPath)
-            print("Deleting old vagrant files...")
-            shutil.rmtree(targetPath)
-        else:
-            print("Auto-copy failed. Manually set up your vagrant folder.")
-            return
-
-    # copy repo vagrant files to local machine.
-    print("Copying new vagrant files...")
-    shutil.copytree(".", targetPath)
-    # Prevent against accidental recursive setup.
-    os.remove(targetPath + "/setup.py")
-    os.rename(targetPath + "/Vagrantfile_Src", targetPath + "/Vagrantfile")
-
-    # navigate to new directory and execute vagrant
-    os.chdir(targetPath)
+    # execute vagrant and ssh in
     print("Starting vagrant instance...")
     call(["vagrant", "up"])
     call(["vagrant", "ssh"])
