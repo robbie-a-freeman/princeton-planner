4c2a3ed72d60f62bc58d3299a42ef9cf9d81707a
wsweeny <wsweeny@princeton.edu>
2018-04-05 13:24:25 -0400

    Added system-independent vagrant setup script setup.py

---
+++ vagrant/setup.py
@@ -0,0 +1,28 @@
+import shutil
+import os
+from subprocess import call
+
+targetPath = "./../../vagrant"
+
+def main():
+    # overwrite existing vagrant files.
+    if (os.path.isdir(targetPath)):
+        reply = input("The vagrant folder already exists. Overwrite? (y/n)")
+        if (reply == 'y' or reply == 'Y'):
+            # print(targetPath)
+            print("Deleting old vagrant files...")
+            shutil.rmtree(targetPath)
+        else:
+            print("Auto-copy failed. Manually set up your vagrant folder.")
+            return
+
+    # copy repo vagrant files to local machine.
+    print("Copying new vagrant files...")
+    shutil.copytree(".", targetPath)
+
+    # navigate to new directory and execute vagrant
+    os.chdir(targetPath)
+    print("Starting vagrant instance...")
+    call(["vagrant", "up"])
+
+main()
