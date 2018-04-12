8175d4bdb86a1df3b363bef0dae50cdd5ba65368
wsweeny <wsweeny@princeton.edu>
2018-04-12 12:20:03 -0400

    Flask app works (slightly) but POST still elusive

--- post-test/landing.py
+++ post-test/landing.py
@@ -1,13 +1,16 @@
 import cgi, cgitb
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/vagrant/post-test', methods = ["POST"])
 def process():
     return 'Hello, World!'
 
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def index():
+    if request.method == 'POST':
+        name = request.form['input']
+        return name;
     return render_template('index.html');
     # return ("Hello!")
 
