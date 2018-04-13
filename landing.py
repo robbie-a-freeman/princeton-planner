a2f3e050d0a1ca0a9d4f3623ed796d4abbdd4a8f
wsweeny <wsweeny@princeton.edu>
2018-04-12 23:53:45 -0400

    POST functionality works forrealzies

--- landing.py
+++ landing.py
@@ -2,11 +2,34 @@ import cgi, cgitb
 from flask import Flask, render_template, request
 from query_parser import db_query
 #app = Flask(__name__)
-app = Flask(__name__, template_folder="../web")
+app = Flask(__name__, template_folder="web")
 
+
+# The first argument is the route offered at the end of the URL:
+# e.g. localhost:8040/ ==> "/"
+# e.g. localhost:8040/web ==> "/web/"
+
+# Handle debug request
 @app.route('/vagrant/srv', methods = ["POST"]) #Where 'vagrant/srv' is the local directory of the file.
 def process():
     return 'Hello, World!'
+
+# handle requests for web folder (debug)
+@app.route('/web/')
+def debug_path():
+    return "Path is /web/"
+
+# Handle requests for plan page (might not work)
+@app.route('/plan.html')
+def plan():
+    return render_template('plan.html')
+
+# try handling a fake request to see what happens
+@app.route('/fakedir')
+def fakedir():
+    return "This dir has no associated local folder"
+
+
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
@@ -17,9 +40,9 @@ def index():
         #return db_query(request.form['search_query']) # If search_query is the name attribute of the HTML form.
         #return "Got a post! " + str(name)
 
-        query = request.form['search_query']
-        return "Got a post!" + str(query)
-    return render_template('indexs.html')
+        query = str(request.form['search_query'])
+        return "Got a post!\n<br>\n<br>" + str(db_query(query))
+    return render_template('index.html')
     # return ("Hello!")
 
 
