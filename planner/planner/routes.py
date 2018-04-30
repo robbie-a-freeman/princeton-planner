1076a83a0767085763e04f501f4b46ffeb2ebd7e
Henry Wang <htwang@princeton.edu>
2018-04-30 16:54:49 -0400

    Have functionality down for adding and removing enrolled courses, semesters, and programs

--- planner/planner/routes.py
+++ planner/planner/routes.py
@@ -59,7 +59,10 @@ def userdata():
     if request.method == "GET":
         #user_info.add_program(user['netid'], 'COS BSE', [1, 2, 3])
         #user_info.add_course(user['netid'], 'COS BSE', 'Prerequisites', 'MUS 213')
+        user_info.add_enrolled_course(user['netid'], 'fall18', 'COS 333')
+        user_info.remove_enrolled_course(user['netid'], 'fall18', 'COS 340')
         #user_info.add_semester(user['netid'], 'fall17')
+        #user_info.remove_program(user['netid'], 'COS')
         return str(user_info.user_query(user['netid']))
         #return str(user_info.user_query('test'))
     else:
